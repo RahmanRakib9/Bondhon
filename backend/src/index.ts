d9cda3e465508ff6476ded3d55967f474d191dc6
maruf-rahman007 <s.marufrahman.g@gmail.com>
2024-12-06 10:43:07 +0600

    mongodb connection setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from 'cors';
 import rootRouter from './routes/rootRoute'
-
+import connectDB from './db';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+connectDB();
 
 app.get('/',(req,res)=>{
     res.send("hello from bondhon!")
